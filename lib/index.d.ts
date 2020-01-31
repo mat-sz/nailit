603aa9317693af133917a74e4ad07817e4c164ff
Mat Sz <contact@matsz.dev>
2020-01-31 21:18:36 +0100

    fixed types

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,4 +1,4 @@
-export function nailIt(imageUrl: string, maxSize?: number, cover?: boolean, outputType?: string, outputQuality?: boolean, outputBlob?: true): Promise<Blob>;
-export function nailIt(imageUrl: string, maxSize?: number, cover?: boolean, outputType?: string, outputQuality?: boolean, outputBlob?: false): Promise<string>;
 export function nailIt(imageUrl: string, maxSize?: number, cover?: boolean, outputType?: string, outputQuality?: boolean): Promise<string>;
+export function nailIt(imageUrl: string, maxSize?: number, cover?: boolean, outputType?: string, outputQuality?: boolean, outputBlob?: false): Promise<string>;
+export function nailIt(imageUrl: string, maxSize?: number, cover?: boolean, outputType?: string, outputQuality?: boolean, outputBlob?: true): Promise<Blob>;
 export default nailIt;
